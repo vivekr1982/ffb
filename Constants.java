65331ae5cd3b1ddb53e4481c2aa81bdf011bade6
vivekr1982 <vivekr1982@gmail.com>
2015-12-02 13:31:13 +0530

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,19 @@
+public interface Constants {
+	String STACK_TRACE_ELEMENT_CLASS_NAME = "java.lang.StackTraceElement";
+
+	String CAN_NOT_OVERRIDE_CAUSE_MSG = "Can't overwrite cause";
+
+	String SELF_CAUSATION_NOT_PERMIT_MSG = "Self-causation not permitted";
+
+	String INITIAL_CAUSE_METHOD_NAME = "initCause";
+
+	String GET_CAUSE_METHOD_NAME = "getCause";
+
+	String ERROR_CODE_MSG = "ErrorCode = ";
+
+	String OTHER_INFORMATION_MSG = "OtherInfo = ";
+
+	String ERROR_CODE_OTHER_INFO_SEPERATOR = ": ";
+
+	String CAUSED_BY_MSG = "Caused by: ";
+}
